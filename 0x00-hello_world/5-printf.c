6f1819d2b65e5f614a6d63ef381579f6c000c77e
edgar9404 <3924@holbertonschool.com>
2021-10-03 22:02:13 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * printf: function that output display string
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
  *
  */
-
-int main(void);
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
