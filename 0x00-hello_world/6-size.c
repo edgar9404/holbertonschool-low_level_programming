168704177bdca2972111a0509f598c50b7f605aa
edgar9404 <3924@holbertonschool.com>
2021-10-03 22:47:09 -0700

    Sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * main - start function of the program
+ *
+ * return: (0)
+ *
+ */
+int main(void)
+{
+	int charSize = sizeof(char);
+	int doubleSize = sizeof(double);
+	int floatSize = sizeof(float);
+
+
+	printf("size of a char: %d byte(s)\n", charSize);
+	printf("size of a double: %d", doubleSize);
+	printf("size of a float: %d", floatSize);
+
+	return (0);
+}
