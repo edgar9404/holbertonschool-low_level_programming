75feb11079484ba729547fb856893ea660d96df1
edgar9404 <3924@holbertonschool.com>
2021-10-04 21:47:20 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet
+ *
+ *
+ * Return: 0 
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
